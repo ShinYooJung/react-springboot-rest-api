dca65395678d3a1d3e3ac492a00f4a9c86eebc9e
shinyoojung <shinyoojung0@gmail.com>
2023-03-07 20:08:25 +0900

    기본 프로젝트 생성

---
+++ src/main/java/com/example/gccoffee/model/Product.java
@@ -0,0 +1,45 @@
+package com.example.gccoffee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+public class Product {
+    private final UUID productId;
+    private String productName;
+    private Category category;
+    private long price;
+    private String description;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Product(UUID productId, String productName, Category category, long price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
